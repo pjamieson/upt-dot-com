cfa9ea6ed9f689866bbf0663a27dd496a023d9a5
Patrick Jamieson <patrick@cleancodecorp.com>
2023-07-23 20:30:09 -0700

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react"
 import { navigate } from "gatsby"
 
-export default function NotFoundPage = () => {
+export default function NotFoundPage() {
 
   useEffect (() => {
     navigate("/"); // redirecting to home page
